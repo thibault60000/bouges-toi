1a7af4421c515a80ff1d8b7af889f6b0b18035ef
thibault60000 <thibault60000@gmail.com>
2019-09-04 18:55:40 +0200

    Add autocomplete communes

--- frontend/components/Articles/Communes.js
+++ frontend/components/Articles/Communes.js
@@ -1,28 +1,101 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Autosuggest from "react-autosuggest";
+import styled from "styled-components";
 import { API_COMMUNES } from "../../config";
-import { Button } from 'antd';
+// DOC : https://github.com/moroshko/react-autosuggest#installation
+
+const StyledAutoSuggest = styled.div`
+  .react-autosuggest__input {
+    border-radius: 0;
+  }
+  .react-autosuggest__suggestions-list {
+    background-color: white;
+    margin-top: 0.5rem;
+    list-style: none;
+    padding: 0.8rem;
+  }
+  .react-autosuggest__suggestion:hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+`;
 
 export default class Communes extends Component {
-  componentDidMount() {
-    axios
-      .get(API_COMMUNES)
-      .then(response => this.setState({ communes: response.data }));
-  }
   state = {
-    communes: []
+    communes: [],
+    suggestions: [],
+    value: ""
   };
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.getSuggestions(value);
+  };
+
+  getSuggestionValue = suggestion => suggestion.nom;
+
+  renderSuggestion = suggestion => <div>{suggestion.nom}</div>;
+
+  getCommunes = inputValue => {
+    return axios
+      .get(
+        `https://geo.api.gouv.fr/communes?nom=${inputValue}&fields=nom,code&format=json&geometry=centre`
+      )
+      .then(response => response.data);
+  };
+
+  getSuggestions = value => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    if (inputLength < 4) {
+      return [];
+    } else if (inputLength === 4) {
+      this.getCommunes(inputValue).then(communes => {
+        communes.length
+          ? this.setState({ suggestions: communes, communes })
+          : this.setState({ suggestions: [] });
+      });
+    } else {
+      const communes = this.state.communes.length
+        ? this.state.communes.filter(
+            c => c.nom.toLowerCase().slice(0, inputLength) === inputValue
+          )
+        : [];
+      this.setState({ suggestions: communes });
+    }
+
+    return [];
+  };
+
   render() {
-    console.log(this.state.communes);
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Entrez le nom de la commune",
+      value,
+      onChange: this.onChange
+    };
     return (
-      <div>
-       <Button type="primary">Primary</Button>
-    <Button>Default</Button>
-    <Button type="dashed">Dashed</Button>
-    <Button type="danger">Danger</Button>
-    <Button type="link">Link</Button>
-    <Button type="primary" shape="round" icon="download" size="large" />
-      </div>
+      <StyledAutoSuggest>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={this.renderSuggestion}
+          inputProps={inputProps}
+        />
+      </StyledAutoSuggest>
     );
   }
 }
